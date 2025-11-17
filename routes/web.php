0a10ac4e45a64124f3a93890c5c1ff3a0929f924
Victor Nnamani <victornnamani651@gmail.com>
2025-11-17 01:19:20 +0100

    Refactor routes to use VideoController for handling video-related actions

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\VideoController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [VideoController::class, 'index'])->name('videos.index');
+Route::post('/videos', [VideoController::class, 'store'])->name('videos.store');
+Route::get('/videos/{video}', [VideoController::class, 'show'])->name('videos.show');
+Route::get('/videos/{video}/status', [VideoController::class, 'status'])->name('videos.status');
+Route::get('/videos/{video}/download', [VideoController::class, 'download'])->name('video.download');
